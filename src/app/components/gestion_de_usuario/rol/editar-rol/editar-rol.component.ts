0c67a5492df50507abee9119cb06f6c10adfcb21
Andres035 <benitoandrescalle035@gmail.com>
2025-05-02 14:17:30 -0400

    Cambio de nevegacion panel de control

--- src/app/components/gestion_de_usuario/rol/editar-rol/editar-rol.component.ts
+++ src/app/components/gestion_de_usuario/rol/editar-rol/editar-rol.component.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-editar-rol',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './editar-rol.component.html',
-  styleUrl: './editar-rol.component.css'
+  styleUrl: './editar-rol.component.css',
 })
 export class EditarRolComponent {
+  constructor(private router: Router) {}
+  Actualizar(): void {
+    this.router.navigate(['/app-panel-control/listar-rol']);
+  }
 
+  Cancelar(): void {
+    this.router.navigate(['/app-panel-control/listar-rol']);
+  }
 }
--- src/app/components/gestion_de_usuario/rol/registrar-rol/registrar-rol.component.ts
+++ src/app/components/gestion_de_usuario/rol/registrar-rol/registrar-rol.component.ts
@@ -1,11 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registrar-rol',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './registrar-rol.component.html',
-  styleUrl: './registrar-rol.component.css'
+  styleUrl: './registrar-rol.component.css',
 })
 export class RegistrarRolComponent {
+  constructor(private router: Router) {}
 
+  registrar(): void {
+    this.router.navigate(['/app-panel-control/listar-rol']);
+  }
+
+  cancelar(): void {
+    this.router.navigate(['/app-panel-control/listar-rol']);
+  }
 }
